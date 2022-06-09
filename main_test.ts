2bb33f22632a5621b8ad7cd26b41ba403e83ce9a
Zachary Carlin <zacarlin@gmail.com>
2022-06-09 19:18:04 -0400

    check for files in post folder

--- main_test.ts
+++ main_test.ts
@@ -1,1 +1,6 @@
-console.log('test')
+import { assertExists } from "https://deno.land/std/testing/asserts.ts";
+Deno.test("Check that articles exist", async () => {
+  // look for files in the posts directory
+  let articles = await Deno.readDir("./posts");
+  assertExists(articles);
+});
