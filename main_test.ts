4cfd4cc3b626933b587b648b46ad8f30238976b1
Zachary Carlin <zacarlin@gmail.com>
2022-06-09 19:21:51 -0400

    const in test

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,6 @@
 import { assertExists } from "https://deno.land/std/testing/asserts.ts";
 Deno.test("Check that articles exist", async () => {
   // look for files in the posts directory
-  let articles = await Deno.readDir("./posts");
+  const articles = await Deno.readDir("./posts");
   assertExists(articles);
 });
